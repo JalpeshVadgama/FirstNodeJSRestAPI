04432a58405a5a9b614d56bc9229974fc56d5124
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2020-05-13 00:44:54 +0530

    Post API for node

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 let port = process.env.port || 3000;
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 let router = express.Router();
 app.use('/api/student', router);
+
 app.listen(port, () => {
     console.log(`Node.js application running on port : ${port}`);
 });
@@ -34,6 +38,38 @@ router.get('/:Id', (req, res) => {
     if (currentStudent) {
         res.json(currentStudent);
     } else {
+        res.sendStatus(404);
+    }
+});
+
+validateStudent = (student) => {
+    if (!student.StudentId) {
+        return 'Student Id is not found.';
+    }
+    if (!student.FirstName) {
+        return 'Student firstname not found.';
+    }
+    if (!student.LastName) {
+        return 'Student lastname not found.';
+    }
+    if (!student.Gender) {
+        return 'Student gender not found.';
+    }
+    return '';
+};
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    let student = req.body;
+    let isValid = validateStudent(student);
+    console.log(isValid);
+    if (isValid == '') {
+        students.push(student);
+        res.write(students);
+        res.statusMessage = 'Student sucessfully added';
+        res.sendStatus(204);
+    } else {
+        res.statusMessage = isValid;
         res.sendStatus(404);
     }
 });
